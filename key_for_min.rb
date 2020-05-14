ccc7b78ba2df2c993b2f8d4ccf6ccd918d068747
Tyler Firth <tyfirth@gmail.com>
2020-05-14 19:49:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
   
   name_hash.each do |key, value|
-    
+    if lowest_value == 0 || lowest_value < value
+      lowest_value = value
   end
 end
